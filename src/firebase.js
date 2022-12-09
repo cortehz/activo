6af4aa103a8f193cb54c490fb7a8e0b2cf04b13c
Samuel Omanchi <omanchis@gmail.com>
2022-12-09 05:48:16 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAdAgUe2ia5B489Ne-Zpuvm8ZAUk7PrGhY",
-    authDomain: "activo-ce6c2.firebaseapp.com",
-    databaseURL: "https://activo-ce6c2.firebaseio.com",
-    projectId: "activo-ce6c2",
-    storageBucket: "activo-ce6c2.appspot.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
     messagingSenderId: "823219410348",
-    appId: "1:823219410348:web:69eb3e8a20f6cdc6c09609",
-    measurementId: "G-XY79GP680G"
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
